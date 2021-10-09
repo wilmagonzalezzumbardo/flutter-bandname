1f28920d2f8f04a390d7500261569f88f05ed924
unknown <miguel_moreno20@hotmail.com>
2021-10-08 21:22:05 -0500

    Fin de la seccion 2a

---
+++ lib/pages/prueba.dart
@@ -0,0 +1,19 @@
+Widget _bandTile(Band band) {
+    return Dismissible(
+      key: Key(band.id),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction) {
+        print('procede el eliminado ${band.name}');
+      },
+      background: Container(
+        padding: EdgeInsets.only(left: 8.0),
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Borrar banda de música??',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      child: 
